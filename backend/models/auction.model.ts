bf4f2ac52578730af92b3cb86b4c46f7a240edad
Seyi-Banjo <elijahbanjo@gmail.com>
2023-04-12 23:29:17 +0100

    Auction Model

---
+++ backend/models/auction.model.ts
@@ -0,0 +1,12 @@
+
+export interface Auction{
+    image: string,
+    item : string,
+    auctionType: string,
+    id: string,
+    categroy: string,
+    date: Date,
+    winner: string,
+    startingPrice: number,
+    leadingBid: number
+}
