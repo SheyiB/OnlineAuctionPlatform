a1a636c51e07ded5fa9a12310051c46a95078061
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-06 23:16:45 +0100

    Setup

--- backend/server.ts
+++ backend/server.ts
@@ -1,4 +1,17 @@
 import express from 'express';
+import * as dotenv from "dotenv";
+import {db} from './db';
+
+dotenv.config();
+db();
+
+
+if (!process.env.PORT){
+    console.log("Environmental Variables not found")
+    process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 
