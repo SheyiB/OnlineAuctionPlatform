16358b298de9afaab3bb73b88f3ccaf9cde26d92
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-07 23:56:04 +0100

    Market Model

--- backend/models/market.model.ts
+++ backend/models/market.model.ts
@@ -1,7 +1,20 @@
 import {Auction} from './auction.model'
-import {Bidders} from './bidder.model'
+import {Schema, model} from 'mongoose';
+
 
 export interface Market{
     auction : [Auction],
     id: string
 }
+
+
+const marketSchema = new Schema<Market>({
+    id: {type: String, required: true},
+    auction: [{type: Schema.Types.ObjectId, ref:'Auction'}],
+},
+{
+    timestamps: true
+})
+
+export const Market = model<Market>('Market', marketSchema )
+
