fc4634c5a10b3cab51e6527908bd1ebfb4550484
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-07 23:53:11 +0100

    Bidder Model Update

--- backend/models/bidder.model.ts
+++ backend/models/bidder.model.ts
@@ -1,6 +1,20 @@
+import {Schema, model} from 'mongoose';
 
 export interface Bidders {
     id: string,
     email: string,
     bid: number
 }
+
+
+const bidderSchema = new Schema<Bidders>({
+    id: {type: String, required: true},
+    email: {type: String, required: true},
+    bid: {type: Number, required: true},
+},
+{
+    timestamps: true
+})
+
+export const Bidder = model<Bidders>('Bidders', bidderSchema)
+
