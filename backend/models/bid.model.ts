3d380c11cfa44547a8901c83c79f0aba8dd19384
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-07 23:49:35 +0100

    Bid Model update

--- backend/models/bid.model.ts
+++ backend/models/bid.model.ts
@@ -1,9 +1,26 @@
-
+import {Schema, model} from 'mongoose';
+import { Auction } from './auction.model';
 
 export interface BidModel{
     bidOwner: string;
     bidId: string;
     bidValue: number;
-    auctionId: string;
+    auctionId: Auction;
     bidTime: Date
 }
+
+
+const bidSchema = new Schema<BidModel>({
+    bidOwner: {type: String, required: true},
+    bidId: {type: String, required: true},
+    auctionId: {type: Schema.Types.ObjectId, ref: 'Auction',  required: true},
+    bidTime: {type: Date, required: true},
+    bidValue: {type: Number, required: true},
+    
+   
+},
+{
+    timestamps: true
+})
+
+export const Bid = model<BidModel>('Bid', bidSchema)
