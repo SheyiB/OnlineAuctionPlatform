22f1e8473892117ed54f2d9cc7a68b3604289079
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-26 23:24:16 +0100

    Auth Controller

--- backend/controllers/auth.ts
+++ backend/controllers/auth.ts
@@ -1,7 +1,24 @@
-export const signUp = ()=>{
-    return `Creating Auctioneer`
+import {AuthService} from '../services/auth.service'
+import {Request, Response} from "express";
+
+const auth = new AuthService();
+
+export const signUp = async (req: Request, res: Response) =>{
+    try{
+        const auctioneer = await auth.signUp(req.body);
+        return res.status(201).json(auctioneer)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
 }
 
-export const login = ()=>{
-    return `Creating Auctioneer`
+export const login = async (req: Request, res: Response) =>{
+    try{        
+        const {auctioneer, token } = await auth.login(req.body);       
+        res.header('x-auth-token', token);
+        return res.status(201).json({auctioneer, token});
+    }  
+    catch(e: any) {
+            return res.status(e.status).json({success: false, message: e.message})            
+    }
 }
