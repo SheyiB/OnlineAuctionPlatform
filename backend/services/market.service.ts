1710a99e7fc1bb5ddf45703f57993a72b20dd88d
Seyi-Banjo <elijahbanjo@gmail.com>
2023-05-27 23:58:00 +0100

    Market Service Update 🚀️

--- backend/services/market.service.ts
+++ backend/services/market.service.ts
@@ -1,9 +1,46 @@
-import {Market, } from '../models/market.model'
+import { resolve } from 'path'
+import {Market, MarketType} from '../models/market.model'
+
 
 export class MarketServie{
 
-    createMarket (body ){
+    createMarket (body: MarketType ){
+        return new Promise<{market: MarketType}>(async(resolve, reject) =>{
+            try{
+                const market: MarketType = await Market.create(body);
 
+                return resolve({market})
+            }
+            catch(e: any){
+                if(e.message.includes('validation failed')){
+                    return reject({code: 400, message: e.message})
+                }
+                
+                e.source = 'Create Market Service';
+                return reject(e)
+            }
+
+        })
+    }
+
+
+    deleteMarket (id: string ){
+        return new Promise<{}>(async(resolve, reject) =>{
+            try{
+                const market: []| null = await Market.findByIdAndDelete(id);
+
+                return resolve({market})
+            }
+            catch(e: any){
+                if(e.message.includes('validation failed')){
+                    return reject({code: 400, message: e.message})
+                }
+                
+                e.source = 'Delete Market Service';
+                return reject(e)
+            }
+
+        })
     }
 
 }
