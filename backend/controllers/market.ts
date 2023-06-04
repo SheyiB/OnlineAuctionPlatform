3b8899d7ef40273e9cb0c51dcec57ebbc6e9e587
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-04 20:54:48 +0100

    Market Controller - Update Market🔥️

--- backend/controllers/market.ts
+++ backend/controllers/market.ts
@@ -14,6 +14,17 @@ export const createMarket = async (req: Request, res: Response) =>{
     }
 }
 
+export const updateMarket = async (req: Request, res: Response) =>{
+    try{
+        const auctioneer = await market.updateMarket(req.params.id, req.body);
+        return res.status(201).json(auctioneer)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+}
+
+
+
 export const deleteMarket = async (req: Request, res: Response) =>{
     try{
         await market.deleteMarket(req.body);       
