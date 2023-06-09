0aa183a721f6fd29c0394ab501f6a36117757fd7
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-09 21:21:57 +0100

    Auctioneer Model and Controller Update

---
+++ backend/controllers/auctioneer.ts
@@ -0,0 +1,53 @@
+import {AuctionServie} from '../services/auction.service';
+
+import {MarketServie} from '../services/market.service';
+
+
+
+import {Request, Response} from "express";
+
+const Auction = new AuctionServie();
+
+const Market = new MarketServie();
+
+export const createMarket = async (req: Request, res: Response) =>{
+    try{
+        const market = await Market.createMarket(req.body);
+        return res.status(201).json(market)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+}
+
+export const createAuction = async (req: Request, res: Response) =>{
+    try{
+        const auction = await Auction.createAuction(req.body);
+        return res.status(201).json(auction)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+
+}
+
+export const getAuction = async (req: Request, res: Response) =>{
+    try{
+        const auction = await Auction.getAuction(req.params.id);
+        return res.status(201).json(auction)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+
+}
+
+export const deleteAuction = async (req: Request, res: Response) =>{
+    try{
+        const auction = await Auction.deleteAuction(req.params.id);
+        return res.status(204).json(auction)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+
+}
+
+
+
