32c3e718900c4586262da635d1c785c8734a0678
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-09 21:36:32 +0100

    Market Service Update 🛠️

--- backend/services/market.service.ts
+++ backend/services/market.service.ts
@@ -62,7 +62,26 @@ export class MarketServie{
         })
     }
 
-    getMarket (id: string ){
+    getAllMarkets (){
+        return new Promise<{market: MarketType[]| null}>(async(resolve, reject) =>{
+            try{
+                const market: MarketType[] | null = await Market.find();
+
+                return resolve({market})
+            }
+            catch(e: any){
+                if(e.message.includes('validation failed')){
+                    return reject({code: 400, message: e.message})
+                }
+                
+                e.source = 'Get Market Service';
+                return reject(e)
+            }
+
+        })
+    }
+
+    getAuctioneerMarket (id: string ){
         return new Promise<{market: MarketType| null}>(async(resolve, reject) =>{
             try{
                 const market: MarketType | null = await Market.findById(id);
