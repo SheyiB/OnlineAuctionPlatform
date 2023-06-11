4e816d3e52c0587549776fd9eb8d8307a909411e
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-11 14:41:01 +0100

    Auction Controller Update - Get Auctioneers Auction 💫️

--- backend/controllers/auction.ts
+++ backend/controllers/auction.ts
@@ -24,6 +24,16 @@ export const getAuction = async (req: Request, res: Response) =>{
 
 }
 
+export const getAuctioneerAuction = async (req: Request, res: Response) =>{
+    try{
+        const auction = await Auction.getAuctioneersAuctions(req.params.id);
+        return res.status(201).json(auction)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+
+}
+
 export const deleteAuction = async (req: Request, res: Response) =>{
     try{
         const auction = await Auction.deleteAuction(req.params.id);
