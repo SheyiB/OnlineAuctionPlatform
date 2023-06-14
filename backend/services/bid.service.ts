cb3bb48e98f1d849aa0c33e01e5f6a632bc88243
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-14 23:23:59 +0100

    Bid Service Initialise (make Bid) 🚀️

---
+++ backend/services/bid.service.ts
@@ -0,0 +1,16 @@
+import {BidModel, Bid} from '../models/bid.model'
+
+export class BidService{
+
+    makeBid(bidDetails: BidModel){
+        return new Promise<{}>(async(resolve, reject) => {
+            try{
+
+            }
+            catch(e: any){
+                
+            }
+
+        })
+    }
+}
