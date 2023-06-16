8de7f110bf80031d11fa6388296982d71adac18f
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-16 21:03:07 +0100

    Bid Service update (Update Bid) 🚀️

--- backend/services/bid.service.ts
+++ backend/services/bid.service.ts
@@ -3,14 +3,44 @@ import {BidModel, Bid} from '../models/bid.model'
 export class BidService{
 
     makeBid(bidDetails: BidModel){
-        return new Promise<{}>(async(resolve, reject) => {
+        return new Promise<{bid: BidModel}>(async(resolve, reject) => {
             try{
                 const bid = await Bid.create(bidDetails)
 
-                resolve(bid)
+                resolve({bid})
             }
             catch(e: any){
+                if(e.message.includes('validation failed')){
+                    return reject({code: 400, message: e.message})
+                }
                 
+                e.source = 'Create Bid Service';
+                return reject(e)
+            }
+
+        })
+    }
+
+    updateBid(bidDetails: BidModel, bidId: string){
+        return new Promise<{bid: BidModel| null}>(async(resolve, reject) => {
+            try{
+                let bid = await Bid.findById(bidDetails)
+
+                if (!bid){
+                    reject({code: 404, message: 'BID NOT FOUND'})
+                }
+
+                bid = await Bid.findByIdAndUpdate(bidId, bidDetails, {new: true, runValidators: true})
+
+                resolve({bid})
+            }
+            catch(e: any){
+                if(e.message.includes('validation failed')){
+                    return reject({code: 400, message: e.message})
+                }
+                
+                e.source = 'Update Bid Service';
+                return reject(e)
             }
 
         })
