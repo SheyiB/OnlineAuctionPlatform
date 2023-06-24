50b89c4d85eaac2801b68221b66e04d81a771390
Seyi-Banjo <elijahbanjo@gmail.com>
2023-06-24 23:28:00 +0100

    Bid Controller - Create Bid🚀️

---
+++ backend/controllers/bid.ts
@@ -0,0 +1,16 @@
+import {Request, Response} from 'express'
+
+import {BidService} from '../services/bid.service';
+
+const Bid = new BidService();
+
+export const makeBid = async (req: Request, res: Response) =>{
+    try{
+        const bid = await Bid.makeBid(req.body);
+        return res.status(201).json(bid)
+    }  catch(e : any){
+        return res.status(e.code? e.code : 500).json({success: false, message: e.message})
+    }
+
+}
+
