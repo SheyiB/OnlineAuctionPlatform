91379a7c1f8985e0a95d0768755507b68659ce49
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-01 21:46:38 +0100

    Bid Controller Update🔰️

--- backend/controllers/bid.ts
+++ backend/controllers/bid.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express'
 
 import {BidService} from '../services/bid.service';
 
-import {validateBidCreation} from '../validators/bid.validator'
+import {validateBidCreation, validateBidUpdate} from '../validators/bid.validator'
 
 const Bid = new BidService();
 
@@ -32,6 +32,10 @@ export const getBid = async (req: Request, res: Response) =>{
 
 export const updateBid = async (req: Request, res: Response) =>{
     try{
+        const {error} = validateBidCreation(req.body)
+        if(error){
+            return res.status(400).json({success: false, message: error.details[0].message})
+        }
         const bid = await Bid.updateBid(req.body, req.params.id);
         return res.status(201).json(bid)
     }  catch(e : any){
