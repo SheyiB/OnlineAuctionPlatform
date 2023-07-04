250555f8f768f34243651ba22bddb6fe37a2a1bc
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-04 23:17:05 +0100

    Auction Validator Update🌌️

--- backend/validators/auction.validator copy.ts
+++ backend/validators/auction.validator copy.ts
@@ -3,15 +3,20 @@ import {AuctioneerType, AuctioneerLogin} from '../models/auctionner.model'
 
 export const validateAuction = ( signupDetails : AuctioneerType) => {
     
-    const signupSchema = Joi.object().keys({
-        firstname: Joi.string().required(),
-        lastname: Joi.string().required(),
-        phone: Joi.number().required(),
-        email :  Joi.string().required(),
-        password: Joi.string().required(),
-        dob: Joi.date().required(),
+    const auctionSchema = Joi.object().keys({
+        image: Joi.string().required(),
+        item: Joi.string().required(),
+        auctionType: Joi.number().required(),
+        id :  Joi.string().required(),
+        category: Joi.string().required(),
+        date: Joi.date().required(),
+        winner: Joi.date().required(),
+        startingPrice: Joi.date().required(),
+        status: Joi.date().required(),
+        leadingBid: Joi.date().required(),
+        bidders: Joi.date().required(),
         market: Joi.string()
     })
 
-    return signupSchema.validate(signupDetails)
+    return auctionSchema.validate(signupDetails)
 }
