93d514d065564e7719d624b5ff4af323a3543b21
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-12 17:21:24 +0100

    Basic Routing 🚀️

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Auction from './pages/auction';
+import AuctionerDashboard from './pages/auctioneerDashboard';
+import Login from './pages/login';
+import Market from './pages/market';
+import Signup from './pages/signup';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const App = () => {
+ return (
     <>
-      <div>
-             </div>
-      
+       <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/dashboard" element={<AuctionerDashboard />} />
+          <Route path="/auction" element={<Auction />} />
+       </Routes>
     </>
-  )
-}
+ );
+};
 
-export default App
+export default App;
