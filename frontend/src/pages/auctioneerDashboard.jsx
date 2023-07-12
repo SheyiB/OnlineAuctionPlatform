9b1f2fe6a09e8187dca49997e8b150002e439f56
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-12 18:48:54 +0100

    Auctioneer Dashboard🚀️

--- frontend/src/pages/auctioneerDashboard.jsx
+++ frontend/src/pages/auctioneerDashboard.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 
 const AuctioneerDashboard  = () => {
     const [create, setCreate] = useState(false) 
+    const [mode, setMode] = useState('basic')
     const markets = [ { name: "Shoe Auction", id: "asjsj822h291opasj9", image: "shoe.png", details: "An Online Auction Store for making Shoes"}, { name: "Antique Auction", id: "asjsj822h291o34a23a9", image: "antique.png", details: "An Online Auction Store for selling Antiques"}]
     const toggleCreateMarket = () => {
          create ? setCreate(false) : setCreate(true)
@@ -11,16 +12,38 @@ const AuctioneerDashboard  = () => {
     return (
     <div>
         <h2> Seyi's Auction</h2>
-        <div>
+        { mode == 'basic' ?
+        <>
+            <div>
             <h1> Markets </h1>
-
             {markets.map(market => <MarketComponent details={market.details} id={market.id} image={market.image} name={market.name} key={market.id} />)}
-        </div>
+            </div>
 
-        <button onClick={toggleCreateMarket}>{create? "Cancel" : "Create Market"}  </button>
+            <button onClick={toggleCreateMarket}>{create? "Cancel" : "Create Market"}  </button>
+            <span> {create ? <NewMarket /> : "" } </span>
+          
+        </> : "" 
+        }
 
-        <span> {create ? <NewMarket /> : "" } </span>
+        {
+            mode == 'view' ? 
+            <>
+
+                <div>
+                    <h1> View Auction </h1>
+                </div>
+            </>  : ""
+        }
+
+        {
+            mode == 'edit' ? ""  : ""
+        }
+
     </div>
+        
+    
+    
+
     );
    };
    
