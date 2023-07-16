cfbdf24dd4590aaaba29e7c06a6dff13995e340d
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-16 15:08:14 +0100

    Login Page Functionality🔰️

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 const Login  = () => {
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
     return (
     <div>
     <h1>YourAuction</h1>
     <h2>Login</h2>
     <form>
         <label>Email</label>
-        <input type="email" /><br/>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} /><br/>
         <label>Password</label>
-        <input type="password" /><br/>
+        <input type="password" onChange={(e) => setPassword(e.target.value)} /><br/>
         <button type="submit"> Login</button>
         
     </form>
