2b1f37b9cef0c730f1b999f15357a5c466f23315
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-16 15:08:46 +0100

    Login Page Functionality-Update 🔰️

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,13 +1,52 @@
 import { useState } from "react";
+import { Link , useNavigate } from 'react-router-dom';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
+
+dotenv.config();
+
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
 
 const Login  = () => {
     const [password, setPassword] = useState('')
     const [email, setEmail] = useState('')
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const auctioneer = {email, password}
+        try {
+          const response = await fetch(`${apiUrl}/auth/login`,{
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json',
+            },
+            body: JSON.stringify(auctioneer),
+        })
+        .then(d => d.json())
+        
+        .catch((e)=>{console.log(e)})
+         
+        const decoded = jwt.verify(response.token, apiKey.toString('utf-8'))
+        let auctioneerdata = await fetch(`${apiUrl}/market/${decoded.id}`).then(d=> d.json())
+        auctioneerdatadata.password = ''
+        localStorage.setItem("auctioneer", auctioneerdata)
+        navigate('/dashboard')
+      } catch (error) {
+        
+          console.error(error);
+        }
+      };
+      
+
+    
     return (
     <div>
     <h1>YourAuction</h1>
     <h2>Login</h2>
-    <form>
+    <form onSubmit={handleSubmit}>
         <label>Email</label>
         <input type="email" onChange={(e) => setEmail(e.target.value)} /><br/>
         <label>Password</label>
