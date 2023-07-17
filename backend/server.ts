94694e19014fabc73b9bd8c7ab9ea924c51ebcf2
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-17 15:17:03 +0100

    Added Morgan to Backend ⚜️

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,8 @@
 import express from 'express';
 import * as dotenv from "dotenv";
 import {db} from './db';
-import cors from 'cors'
+import cors from 'cors';
+import morgan from 'morgan'
 
 //import routes
 import {authRouter} from './routes/auth.routes'
@@ -22,14 +23,18 @@ if (!process.env.PORT){
     process.exit(1);
 }
 
+
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
 
 const app = express();
 
+
 app.use(cors({credentials: true}));
 
 app.use(express.json());
+
+app.use(morgan('dev')); 
 
 app.use('/auction-api/auth',authRouter)
 app.use('/auction-api/market',marketRouter)
