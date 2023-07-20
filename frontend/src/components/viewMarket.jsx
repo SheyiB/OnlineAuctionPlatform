2676ea1f3c379941bd253eceb2c46cee72a36118
Seyi-Banjo <elijahbanjo@gmail.com>
2023-07-20 09:58:13 +0100

    View Market Component-Styling  🔰️

--- frontend/src/components/viewMarket.jsx
+++ frontend/src/components/viewMarket.jsx
@@ -1,20 +1,29 @@
-const ViewMarketComponent = ({image, item, auctionType, startingPrice, aucionDetails, date}) => {
+const ViewMarketComponent = ({image, item, auctionType, startingPrice, auctionDetails, date}) => {
     return(
         <>
-        <div>
-            <image src={image} />
-            <div>
-                <h2>{item} </h2>
-                <span>{auctionType}</span>
-                <span>{date}</span>
-                <span>  {auctionType !== "Free Penny" ? `Starting Bid : ${auctionType == 'english' || auctionType == 'duth' ? startingPrice : "No Starting Bid"}` : ` Bid Increment : ${startingPrice}`} </span>
-                <p>{aucionDetails}</p>
+        <div className="view-market-container">
+            <img src={image} alt="Market Item" className="view-market-image" />
+            <div className="view-market-details">
+            <h2 className="view-market-heading">{item}</h2>
+            <span className="view-market-subheading">{auctionType}</span>
+            <span className="view-market-subheading">{date}</span>
+            <span className="view-market-subheading">
+                {auctionType !== "Free Penny"
+                ? `Starting Bid: ${
+                    auctionType === "english" || auctionType === "dutch"
+                        ? startingPrice
+                        : "No Starting Bid"
+                    }`
+                : `Bid Increment: ${startingPrice}`}
+            </span>
+            <p className="view-market-description">{auctionDetails}</p>
             </div>
-            <div>
-                <button>Edit</button>
-                <button>Delete</button>
+            <div className="view-market-buttons">
+            <button className="view-market-button">Edit</button>
+            <button className="view-market-button">Delete</button>
             </div>
         </div>
-        </>
+</>
+
     )
 }
