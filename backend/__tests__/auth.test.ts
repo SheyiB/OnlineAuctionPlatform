8a8b7482085dcef1c7f841343c1c9eb3f66ece17
Seyi-Banjo <elijahbanjo@gmail.com>
2023-08-01 23:51:55 +0100

    Auth Routes Tests🧪️

--- backend/__tests__/auth.test.ts
+++ backend/__tests__/auth.test.ts
@@ -14,13 +14,29 @@ afterEach(async () => {
     await mongoose.connection.close();
 })
 
+
+// "image" : "../assets/pic.jpg",
+// "item" : "Thors Hammer",
+// "auctionType" : "vickery",
+// "category" : "Rare",
+// "startingPrice" : 0,
+// "date" : "12-08-2023",
+// "market" : "2j13399dnnnd3i9323d",
+// "duration" : 1,
+// "details" : "This is Thor's Hammer gotten from Asgard!"
+
 describe("GET /api/signup", ()=>{
     it("should signup User", async () => {
-        const res = await request(app).post("/api/auth/signup").send({
-            
+        const res = await request(app).post("http://localhost:8080/auction-api/auth/signUp").send({
+        "firstname" : "John",
+        "lastname" : "Doe",
+        "email" : "john@doe.com",
+        "phone" : 123455632,
+        "password" : "user123"
+
         });
         expect(res.statusCode).toBe(201);
-        expect(res.body.name).toBeGreaterThan(0);
+        expect(res.body._id).toBeGreaterThan(0);
     })
 })
 
--- backend/db.ts
+++ backend/db.ts
@@ -1,6 +1,7 @@
 import { connect } from 'mongoose';
 
 export const db = async() =>{
+    console.log("About to connect")
     await connect('mongodb://127.0.0.1:27017/AuctionApp');
 
     console.log(`Server Running on ${process.env.PORT}`)
