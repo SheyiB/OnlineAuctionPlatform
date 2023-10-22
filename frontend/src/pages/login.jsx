29ef545bd8fbf7e32e840199e5d55737a7f69f44
Seyi-Banjo <elijahbanjo@gmail.com>
2023-10-22 23:48:39 +0100

    Added SignUp to Login Page🛠️

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -51,7 +51,8 @@ const Login  = () => {
         <input type="email" onChange={(e) => setEmail(e.target.value)} /><br />
         <label>Password</label>
         <input type="password" onChange={(e) => setPassword(e.target.value)} /><br />
-        <button type="submit">Login</button>
+        <button type="submit">Login</button> <br/> <br/>
+        <span> New User?  <Link to="/signup"> SignUp </Link></span>
       </form>
     </div>
     
